cbbabe61c96c6d0ad7a9a7604147741c290be9b2
Misael <misael.gonzalez.almeida@gmail.com>
2023-10-18 14:21:26 +0200

    Initial Dockerfile

---
+++ docker/start_docker.sh
@@ -0,0 +1,1 @@
+sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -e QT_X11_NO_MITSHM=1 --gpus all person_det/r1_crowd:iron_devel
